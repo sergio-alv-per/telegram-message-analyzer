a130e8422fdb1a176299fe7a98f36778edc686e0
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-01-05 15:57:29 +0100

    Lectura del archivo exportado de Telegram.

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import argparse
+import json
+
+def leer_archivo_exportado(nombre_archivo):
+    """ Lee el archivo JSON generado por Telegram y devuelve un diccionario con los datos."""
+    with open(nombre_archivo, encoding="UTF-8") as archivo:
+        datos_coversaciones = json.load(archivo)
+        return datos_coversaciones
+
+def filtrar_conversaciones_personales(datos_conversaciones):
+    """ Devuelve una lista con las conversaciones personales, es decir, excluye los grupos y otros tipos de conversaciones."""
+    conversaciones_personales = []
+    for conversacion in datos_conversaciones["chats"]["list"]:
+        if conversacion["type"] == "personal_chat":
+            conversaciones_personales.append(conversacion)
+    return conversaciones_personales
+
+def escoger_conversacion(conversaciones_personales):
+    """ Muestra las conversaciones disponibles y devuelve el nombre de la conversación escogida."""
+    print("Conversaciones: ")
+    for i, conversacion in enumerate(conversaciones_personales):
+        print(f"({i}) {conversacion['name']}")
+    
+    num_conversacion_escogida = int(input("Elige una conversación: "))
+
+    return conversaciones_personales[num_conversacion_escogida]["name"]
 
 # Lectura de argumentos por línea de comandos
 parser = argparse.ArgumentParser()
@@ -7,4 +32,20 @@ parser.add_argument("-c", "--chat", help="Nombre de contacto correspondiente a l
 
 args = parser.parse_args()
 
-print(args)
+# Lectura del archivo JSON, quitando las conversaciones que no son personales
+datos_conversaciones = filtrar_conversaciones_personales(leer_archivo_exportado(args.archivo))
+
+# Escoger una conversación y obtener sus datos
+if args.chat:
+    nombre_conversacion = args.chat
+else:
+    nombre_conversacion = escoger_conversacion(datos_conversaciones)
+
+conversacion = None
+for conv in datos_conversaciones:
+    if conv["name"] == nombre_conversacion:
+        conversacion = conv
+        break
+
+print(conversacion)
+
