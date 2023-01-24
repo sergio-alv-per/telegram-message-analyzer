57e3b8db174baea36506fa1afa6a8304f86eb9e0
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-01-24 21:05:13 +0100

    Descartada lógica para escoger conversaciones.

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from preprocesado import preprocesado_archivo_exportado
 from ProcesadorMensajes import ProcesadorMensajes
 
 
+
 # Lectura de argumentos por línea de comandos
 parser = argparse.ArgumentParser()
 parser.add_argument("archivo", help="Archivo a leer, el result.json generado por Telegram.")
-parser.add_argument("-c", "--chat", help="Nombre de contacto correspondiente a la conversación que se quiere analizar.")
 
 args = parser.parse_args()
 
-lista_mensajes = preprocesado_archivo_exportado(args.archivo, nombre_conversacion=args.chat)
+lista_mensajes = preprocesado_archivo_exportado(args.archivo)
 
 datos_conversacion = ProcesadorMensajes(lista_mensajes).procesar_mensajes()
 
