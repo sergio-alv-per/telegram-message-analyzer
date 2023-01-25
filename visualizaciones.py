66591f246efc5a0538d456dac42913e4735698f2
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-01-25 18:20:24 +0100

    Creación del módulo para generar gráficas.

---
+++ visualizaciones.py
@@ -0,0 +1,8 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def generar_visualizaciones(datos_conversacion, analisis_conversacion, directorio="visualizaciones"):
+    generar_grafica_recuentos_mensajes(datos_conversacion["recuentos_mensajes"], directorio)
+
+def generar_grafica_recuentos_mensajes(recuentos_mensajes, directorio):
+    pass
