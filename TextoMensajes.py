aaf4d57b2692a26480f549d8fdc1e961a5f858d7
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-01-25 16:37:46 +0100

    Limpieza del texto para mejor análisis.

--- TextoMensajes.py
+++ TextoMensajes.py
@@ -8,6 +8,9 @@ class TextoMensajes():
         self.frecuencia_palabras = defaultdict(lambda: Counter())
         self.texto_mensajes = defaultdict(lambda: [])
     
+    def quitar_caracteres_especiales(self, texto):
+        return texto.translate({ord(i): None for i in '¡!¿?.,;:"\n\'()[]{}'})
+    
     def obtener_texto_mensaje(self, mensaje):
         """ Procesa el campo "text" de un mensaje. Este campo puede ser un string o una lista de strings y diccionarios.
             Los diccionarios representan texto de tipo especial (negrita, cursiva, links, etc.).
@@ -46,6 +49,7 @@ class TextoMensajes():
             palabras = texto.split()
 
             for palabra in palabras:
+                palabra = self.quitar_caracteres_especiales(palabra)
                 self.frecuencia_palabras[autor_mensaje][palabra] += 1
 
     def obtener_frecuencia_palabras(self):
