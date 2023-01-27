a86875e70995fe88a497ed2845eec9a3b9c25c35
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-01-27 16:36:21 +0100

    Prints para mostrar el progreso del programa.

--- main.py
+++ main.py
@@ -8,16 +8,23 @@ from visualizaciones import generar_visualizaciones
 
 # Lectura de argumentos por línea de comandos
 parser = argparse.ArgumentParser()
-parser.add_argument("archivo", help="Archivo a leer, el result.json generado por Telegram.")
+parser.add_argument("archivo", help="Archivo que leer, el result.json generado por Telegram.")
 
 args = parser.parse_args()
 
+print("Preprocesando...")
 lista_mensajes = preprocesado_archivo_exportado(args.archivo)
 
+print("Procesando...")
 datos_conversacion = ProcesadorMensajes(lista_mensajes).procesar_mensajes()
 
+print("Analizando...")
 analisis_conversacion = analizar_datos_conversacion(datos_conversacion)
 
+print("Exportando...")
 exportar_datos_generados(datos_conversacion, analisis_conversacion)
 
+print("Generando visualizaciones...")
 generar_visualizaciones(datos_conversacion, analisis_conversacion)
+
+print("¡Listo!")
