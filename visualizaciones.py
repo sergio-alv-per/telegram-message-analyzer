314df85741cd68d00a6852df090efc782c7293b2
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-01-27 17:37:24 +0100

    Gráfica de mensajes por día.

--- visualizaciones.py
+++ visualizaciones.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import os
 
 COLOR_FONDO = "#F5F0F6"
-COLOR_BARRAS_EMISOR_1 = "#385F71"
-COLOR_BARRAS_EMISOR_2 = "#D7B377"
+COLOR_EMISOR_1 = "#385F71"
+COLOR_EMISOR_2 = "#D7B377"
+COLOR_TOTALES = "#90323D"
+
 
 def generar_visualizaciones(datos_conversacion, analisis_conversacion, directorio="visualizaciones"):
 
@@ -13,7 +15,12 @@ def generar_visualizaciones(datos_conversacion, analisis_conversacion, directori
         os.makedirs(directorio)
     
     generar_grafica_recuentos_mensajes(datos_conversacion["recuentos_mensajes"], directorio)
+    generar_grafica_mensajes_dia(datos_conversacion["series_tiempo"], directorio)
+    generar_grafica_mensajes_dia_año(datos_conversacion["series_tiempo"], directorio)
     generar_grafica_mensajes_dia_semana(datos_conversacion["series_tiempo"], directorio)
+    generar_grafica_mensajes_hora(datos_conversacion["series_tiempo"], directorio)
+    generar_grafica_mensajes_minuto(datos_conversacion["series_tiempo"], directorio)
+
 
     plt.show()
 
@@ -32,7 +39,7 @@ def generar_grafica_recuentos_mensajes(recuentos_mensajes, directorio):
     emisor_1, emisor_2 = recuentos_mensajes.keys()
     fig.suptitle(f"Recuentos: {emisor_1} y {emisor_2}")
 
-    titulos = ["Mensajes", "Imágenes", "Vídeos", "Stickers", "Notas de voz", "Duración de notas de voz", "Noas de vídeo", "Duración de notas de vídeo"]
+    titulos = ["Mensajes", "Imágenes", "Vídeos", "Stickers", "Notas de voz", "Duración de notas de voz", "Noats de vídeo", "Duración de notas de vídeo"]
     atributos = ["num_mensajes", "num_fotos", "num_videos", "num_stickers", "num_notas_voz", "duracion_notas_voz", "num_notas_video", "duracion_notas_video"]
 
     # Generar las 8 gráficas de barras
@@ -56,8 +63,8 @@ def generar_subplot_barras_horizontales(ax, recuentos_mensajes, atributo):
     ax.axis("off")
 
     # Representar las barras
-    ax.barh(atributo, proporcion_1, color=COLOR_BARRAS_EMISOR_1)
-    ax.barh(atributo, proporcion_2, left=proporcion_1, color=COLOR_BARRAS_EMISOR_2)
+    ax.barh(atributo, proporcion_1, color=COLOR_EMISOR_1)
+    ax.barh(atributo, proporcion_2, left=proporcion_1, color=COLOR_EMISOR_2)
 
     # Etiquetas en las barras
     ax.text(0, -1, f"{recuentos_mensajes[emisor_1][atributo]} ({100*proporcion_1:.2f}%)")
@@ -81,8 +88,40 @@ def generar_grafica_mensajes_dia_semana(series_tiempo, directorio):
 
     # Generar las barras
     dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-    ax.bar(dias_semana, series_tiempo[emisor_1]["mensajes_por_dia_semana"].values, color=COLOR_BARRAS_EMISOR_1)
-    ax.bar(dias_semana, series_tiempo[emisor_2]["mensajes_por_dia_semana"].values, bottom=series_tiempo[emisor_1]["mensajes_por_dia_semana"].values, color=COLOR_BARRAS_EMISOR_2)
+    ax.bar(dias_semana, series_tiempo[emisor_1]["mensajes_por_dia_semana"].values, color=COLOR_EMISOR_1)
+    ax.bar(dias_semana, series_tiempo[emisor_2]["mensajes_por_dia_semana"].values, bottom=series_tiempo[emisor_1]["mensajes_por_dia_semana"].values, color=COLOR_EMISOR_2)
 
     archivo = os.path.join(directorio, "mensajes_dia_semana.png")
     fig.savefig(archivo, dpi=150)
+
+def generar_grafica_mensajes_dia(series_tiempo, directorio, numero_dias=7):
+    """ Genera un gráfico con una línea marcando el número de mensajes total
+        enviados por ambos emisores de media los últimos numero_dias días.
+    """
+    fig, ax = plt.subplots(figsize=(6, 4))
+
+    fig.set_facecolor(COLOR_FONDO)
+    ax.set_facecolor(COLOR_FONDO)
+
+    fig.subplots_adjust(bottom=0.23)
+
+    emisor_1, emisor_2 = series_tiempo.keys() 
+    ax.set_title(f"Mensajes por día (media {numero_dias} días): {emisor_1} y {emisor_2}")
+
+    mensajes_totales = series_tiempo[emisor_1]["mensajes_por_dia"].add(series_tiempo[emisor_2]["mensajes_por_dia"], fill_value=0)
+    media_movimiento = mensajes_totales.rolling(numero_dias).mean()
+    ax.plot(media_movimiento, color=COLOR_TOTALES)
+
+    ax.tick_params(axis='x', rotation=45)
+
+    archivo = os.path.join(directorio, "mensajes_dia.png")
+    fig.savefig(archivo, dpi=150)
+
+def generar_grafica_mensajes_dia_año(series_tiempo, directorio):
+    pass
+
+def generar_grafica_mensajes_hora(series_tiempo, directorio):
+    pass
+
+def generar_grafica_mensajes_minuto(series_tiempo, directorio):
+    pass
