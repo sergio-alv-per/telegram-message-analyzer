24d8bc1604b7cdee1cefdd8ea49682b2d111434b
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-01-27 17:37:12 +0100

    Se asegura que las series se exporten ordenadas.

--- exportado.py
+++ exportado.py
@@ -23,12 +23,12 @@ def exportar_series_tiempo(series_tiempo, nombre_directorio):
     for emisor, series in series_tiempo.items():
         for nombre_serie, serie in series.items():
             archivo = os.path.join(nombre_directorio, f"series_tiempo_{nombre_serie}_{emisor}.csv")
-            serie.to_csv(archivo, encoding="UTF-8", header=False)
+            serie.sort_index().to_csv(archivo, encoding="UTF-8", header=False)
 
 def exportar_frecuencia_palabras(frecuencia_palabras, nombre_directorio):
     for emisor, frecuencias in frecuencia_palabras.items():
         archivo = os.path.join(nombre_directorio, f"frecuencia_palabras_{emisor}.csv")
-        frecuencias.to_csv(archivo, encoding="UTF-8")
+        frecuencias.sort_index().to_csv(archivo, encoding="UTF-8")
 
 def exportar_analisis_conversacion(analisis_conversacion, nombre_directorio):
     archivo = os.path.join(nombre_directorio, "analisis_conversacion.csv")
