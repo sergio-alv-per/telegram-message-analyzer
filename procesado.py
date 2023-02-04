f533dec5e5c28bc6e6b087bbbb2113e580c866d5
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-02-04 21:51:41 +0100

    Mejora de la documentación.

--- procesado.py
+++ procesado.py
@@ -2,7 +2,12 @@ from Recuentos import Recuentos
 from SeriesTiempo import SeriesTiempo
 from TextoMensajes import TextoMensajes
 
+
 def procesar_mensajes(lista_mensajes):
+    """ Procesa una lista de mensajes de Telegram, obteniendo datos de interés.
+        Se devuelven estos datos en un diccionario.
+    """
+
     series_tiempo = SeriesTiempo()
     texto_mensajes = TextoMensajes()
     recuentos = Recuentos()
@@ -13,7 +18,7 @@ def procesar_mensajes(lista_mensajes):
         texto_mensajes.añadir_mensaje(mensaje)
 
         recuentos.añadir_mensaje(mensaje)
-    
+
     return {"recuentos_mensajes": recuentos.obtener_recuentos(),
             "series_tiempo": series_tiempo.obtener_series_tiempo(),
             "frecuencia_palabras": texto_mensajes.obtener_frecuencia_palabras(),
