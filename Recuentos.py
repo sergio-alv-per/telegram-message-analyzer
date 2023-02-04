91e99b05b31927f733ad9872db704833fc0792e0
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-02-05 00:09:58 +0100

    Reestructuración y mejora de legibilidad.

--- Recuentos.py
+++ Recuentos.py
@@ -7,29 +7,38 @@ class Recuentos():
 
     def __init__(self):
         self.recuentos_mensajes = defaultdict(lambda: Counter())
+    
+    def actualizar_numero_mensajes(self, autor_mensaje, mensaje):
+        self.recuentos_mensajes[autor_mensaje]["num_mensajes"] += 1
+    
+    def actualizar_numero_fotos(self, autor_mensaje, mensaje):
+        if "photo" in mensaje:
+            self.recuentos_mensajes[autor_mensaje]["num_fotos"] += 1
+    
+    def actualizar_numero_multimedia(self, autor_mensaje, mensaje):
+        """ Actualiza los recuentos en el caso de que el mensaje sea contenido
+            multimedia (videos, stickers, notas de voz o de vídeo).
+        """
+        recuentos_autor_mensaje = self.recuentos_mensajes[autor_mensaje]
+
+        if "media_type" in mensaje:
+            if mensaje["media_type"] == "video_file":
+                recuentos_autor_mensaje["num_videos"] += 1
+            elif mensaje["media_type"] == "sticker":
+                recuentos_autor_mensaje["num_stickers"] += 1
+            elif mensaje["media_type"] == "voice_message":
+                recuentos_autor_mensaje["num_notas_voz"] += 1
+                recuentos_autor_mensaje["duracion_notas_voz"] += mensaje["duration_seconds"]
+            elif mensaje["media_type"] == "video_message":
+                recuentos_autor_mensaje["num_notas_video"] += 1
+                recuentos_autor_mensaje["duracion_notas_video"] += mensaje["duration_seconds"]
 
     def añadir_mensaje(self, mensaje):
         autor_mensaje = mensaje["from"]
 
-        # Diccionario de datos del autor del mensaje, se usará repetidamente
-        diccionario_recuentos = self.recuentos_mensajes[autor_mensaje]
-
-        diccionario_recuentos["num_mensajes"] += 1
-
-        if "photo" in mensaje:
-            diccionario_recuentos["num_fotos"] += 1
-
-        if "media_type" in mensaje:
-            if mensaje["media_type"] == "video_file":
-                diccionario_recuentos["num_videos"] += 1
-            elif mensaje["media_type"] == "sticker":
-                diccionario_recuentos["num_stickers"] += 1
-            elif mensaje["media_type"] == "voice_message":
-                diccionario_recuentos["num_notas_voz"] += 1
-                diccionario_recuentos["duracion_notas_voz"] += mensaje["duration_seconds"]
-            elif mensaje["media_type"] == "video_message":
-                diccionario_recuentos["num_notas_video"] += 1
-                diccionario_recuentos["duracion_notas_video"] += mensaje["duration_seconds"]
+        self.actualizar_numero_mensajes(autor_mensaje, mensaje)
+        self.actualizar_numero_fotos(autor_mensaje, mensaje)
+        self.actualizar_numero_multimedia(autor_mensaje, mensaje)
 
     def obtener_recuentos(self):
         """ Devuelve un diccionario con los recuentos de cada participante."""
