79849051bb331b54323a214e8f7f37639fd8123a
sergio-alv-per <55792768+sergio-alv-per@users.noreply.github.com>
2023-02-05 12:40:07 +0100

    Refactorización para mejorar legibilidad.

--- visualizaciones.py
+++ visualizaciones.py
@@ -12,27 +12,42 @@ COLOR_TOTALES = "#90323D"
 
 def generar_visualizaciones(datos_conversacion, analisis_conversacion, directorio="visualizaciones"):
 
-    # Crear el directorio si no existe
+    crear_directorio_si_no_existe(directorio)
+
+    # Necesario para que las fechas se muestren en español
+    establecer_idioma_es()
+
+    recuentos_mensajes = datos_conversacion["recuentos_mensajes"]
+    series_tiempo = datos_conversacion["series_tiempo"]
+    tf_idf_palabras = analisis_conversacion["tf_idf_palabras"]
+
+    generar_grafica_recuentos_mensajes(recuentos_mensajes, directorio)
+
+    generar_grafica_mensajes_dia(series_tiempo, directorio)
+
+    generar_grafica_mensajes_dia_año(series_tiempo, directorio)
+
+    generar_grafica_mensajes_dia_semana(series_tiempo, directorio)
+
+    generar_grafica_mensajes_hora(series_tiempo, directorio)
+
+    generar_grafica_mensajes_minuto(series_tiempo, directorio)
+
+    generar_grafica_tf_idf(tf_idf_palabras, directorio)
+
+
+def crear_directorio_si_no_existe(directorio):
     if not os.path.exists(directorio):
         os.makedirs(directorio)
 
-    # Establecer el idioma para que las fechas se muestren en español
+
+def establecer_idioma_es():
     locale.setlocale(locale.LC_ALL, "es_ES.UTF-8")
 
-    generar_grafica_recuentos_mensajes(
-        datos_conversacion["recuentos_mensajes"], directorio)
-    generar_grafica_mensajes_dia(
-        datos_conversacion["series_tiempo"], directorio)
-    generar_grafica_mensajes_dia_año(datos_conversacion["series_tiempo"], directorio)
-    generar_grafica_mensajes_dia_semana(
-        datos_conversacion["series_tiempo"], directorio)
-    generar_grafica_mensajes_hora(
-        datos_conversacion["series_tiempo"], directorio)
-    generar_grafica_mensajes_minuto(
-        datos_conversacion["series_tiempo"], directorio)
-    generar_grafica_tf_idf(
-        analisis_conversacion["tf_idf_palabras"], directorio)
-
+def guardar_grafica(fig, nombre_archivo, directorio, dpi=150):
+    archivo = os.path.join(directorio, nombre_archivo)
+    fig.savefig(archivo, dpi=dpi)
+    plt.close(fig)
 
 def generar_grafica_recuentos_mensajes(recuentos_mensajes, directorio):
     """ Se genera una gráfica que representa, para cada valor del que se ha hecho recuento,
@@ -59,8 +74,7 @@ def generar_grafica_recuentos_mensajes(recuentos_mensajes, directorio):
         ax.set_title(titulo)
         generar_subplot_barras_horizontales(ax, recuentos_mensajes, atributo)
 
-    archivo = os.path.join(directorio, "recuentos.png")
-    fig.savefig(archivo, dpi=150)
+    guardar_grafica(fig, "recuentos.png", directorio)
 
 
 def generar_subplot_barras_horizontales(ax, recuentos_mensajes, atributo):
@@ -111,8 +125,7 @@ def generar_grafica_mensajes_dia_semana(series_tiempo, directorio):
     ax.bar(dias_semana, series_tiempo[emisor_2]["mensajes_por_dia_semana"].values,
            bottom=series_tiempo[emisor_1]["mensajes_por_dia_semana"].values, color=COLOR_EMISOR_2)
 
-    archivo = os.path.join(directorio, "mensajes_dia_semana.png")
-    fig.savefig(archivo, dpi=150)
+    guardar_grafica(fig, "mensajes_dia_semana.png", directorio)
 
 
 def generar_grafica_mensajes_dia(series_tiempo, directorio, numero_dias=7):
@@ -139,8 +152,7 @@ def generar_grafica_mensajes_dia(series_tiempo, directorio, numero_dias=7):
     ax.xaxis.set_major_formatter(
         mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
 
-    archivo = os.path.join(directorio, "mensajes_dia.png")
-    fig.savefig(archivo, dpi=150)
+    guardar_grafica(fig, "mensajes_dia.png", directorio)
 
 
 def generar_grafica_mensajes_dia_año(series_tiempo, directorio):
@@ -166,8 +178,7 @@ def generar_grafica_mensajes_dia_año(series_tiempo, directorio):
     for label in ax.get_xticklabels(which='major'):
         label.set(rotation=30, horizontalalignment='right')
 
-    archivo = os.path.join(directorio, "mensajes_dia_año.png")
-    fig.savefig(archivo, dpi=150)
+    guardar_grafica(fig, "mensajes_dia_año.png", directorio)
 
 
 def generar_grafica_mensajes_hora(series_tiempo, directorio):
@@ -195,8 +206,7 @@ def generar_grafica_mensajes_hora(series_tiempo, directorio):
     for label in ax.get_xticklabels():
         label.set(rotation=30, horizontalalignment='right')
 
-    archivo = os.path.join(directorio, "mensajes_hora.png")
-    fig.savefig(archivo, dpi=150)
+    guardar_grafica(fig, "mensajes_hora.png", directorio)
 
 
 def generar_grafica_mensajes_minuto(series_tiempo, directorio):
@@ -226,8 +236,7 @@ def generar_grafica_mensajes_minuto(series_tiempo, directorio):
     for label in ax.get_xticklabels(which='major'):
         label.set(rotation=30, horizontalalignment='right')
 
-    archivo = os.path.join(directorio, "mensajes_minuto.png")
-    fig.savefig(archivo, dpi=150)
+    guardar_grafica(fig, "mensajes_minuto.png", directorio)
 
 
 def generar_grafica_tf_idf(tf_idf, directorio, num_por_emisor=5):
@@ -270,5 +279,4 @@ def generar_grafica_tf_idf(tf_idf, directorio, num_por_emisor=5):
         ax.eventplot([mas_relevantes.iloc[i]],
                      color=COLOR_TOTALES, linelength=0.3, linewidths=4)
 
-    archivo = os.path.join(directorio, "palabras.png")
-    fig.savefig(archivo, dpi=150)
+    guardar_grafica(fig, "palabras.png", directorio)
